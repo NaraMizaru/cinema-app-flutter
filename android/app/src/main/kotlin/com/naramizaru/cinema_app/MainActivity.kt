0a4e965d57a534ea004f7b579f443d3066c29940
NaraMizaru <hilal.muhammad0807@gmail.com>
2023-02-05 09:55:07 +0700

    setup project and add splash screen

---
+++ android/app/src/main/kotlin/com/naramizaru/cinema_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.naramizaru.cinema_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/api_constants.dart
@@ -0,0 +1,6 @@
+class ApiConstant {
+  static const String baseUrl = 'https://api.themoviedb.org/3';
+  static const String apiKey = '46fd4d819bacbbba342f23cde01db136';
+  static const String imgOriginal = 'https://image.tmdb.org/t/p/original';
+  static const String imgW500 = 'https://image.tmdb.org/t/p/w500';
+}
